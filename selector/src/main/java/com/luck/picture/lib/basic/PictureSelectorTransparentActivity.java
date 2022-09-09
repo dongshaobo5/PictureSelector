1c515faaa4f0536b2b58b34452b73a80e38af85a
dongshaobo <dongshaobo235860@163.com>
2022-09-09 16:34:12 +0800

    选择图片，上传图片

---
+++ selector/src/main/java/com/luck/picture/lib/basic/PictureSelectorTransparentActivity.java
@@ -0,0 +1,130 @@
+package com.luck.picture.lib.basic;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.luck.picture.lib.PictureOnlyCameraFragment;
+import com.luck.picture.lib.PictureSelectorPreviewFragment;
+import com.luck.picture.lib.PictureSelectorSystemFragment;
+import com.luck.picture.lib.R;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureSelectionConfig;
+import com.luck.picture.lib.entity.LocalMedia;
+import com.luck.picture.lib.immersive.ImmersiveManager;
+import com.luck.picture.lib.manager.SelectedManager;
+import com.luck.picture.lib.style.PictureWindowAnimationStyle;
+import com.luck.picture.lib.style.SelectMainStyle;
+import com.luck.picture.lib.utils.StyleUtils;
+
+import java.util.ArrayList;
+
+/**
+ * @author：luck
+ * @date：2022/2/10 6:07 下午
+ * @describe：PictureSelectorTransparentActivity
+ */
+public class PictureSelectorTransparentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        immersive();
+        setContentView(R.layout.ps_empty);
+        if (isExternalPreview()) {
+            // TODO ignore
+        } else {
+            setActivitySize();
+        }
+        setupFragment();
+    }
+
+    private boolean isExternalPreview() {
+        int modeTypeSource = getIntent().getIntExtra(PictureConfig.EXTRA_MODE_TYPE_SOURCE, 0);
+        return modeTypeSource == PictureConfig.MODE_TYPE_EXTERNAL_PREVIEW_SOURCE;
+    }
+
+    private void immersive() {
+        if (PictureSelectionConfig.selectorStyle == null) {
+            PictureSelectionConfig.getInstance();
+        }
+        SelectMainStyle mainStyle = PictureSelectionConfig.selectorStyle.getSelectMainStyle();
+        int statusBarColor = mainStyle.getStatusBarColor();
+        int navigationBarColor = mainStyle.getNavigationBarColor();
+        boolean isDarkStatusBarBlack = mainStyle.isDarkStatusBarBlack();
+        if (!StyleUtils.checkStyleValidity(statusBarColor)) {
+            statusBarColor = ContextCompat.getColor(this, R.color.ps_color_grey);
+        }
+        if (!StyleUtils.checkStyleValidity(navigationBarColor)) {
+            navigationBarColor = ContextCompat.getColor(this, R.color.ps_color_grey);
+        }
+        ImmersiveManager.immersiveAboveAPI23(this, statusBarColor, navigationBarColor, isDarkStatusBarBlack);
+    }
+
+    private void setupFragment() {
+        String fragmentTag;
+        Fragment targetFragment = null;
+        int modeTypeSource = getIntent().getIntExtra(PictureConfig.EXTRA_MODE_TYPE_SOURCE, 0);
+        if (modeTypeSource == PictureConfig.MODE_TYPE_SYSTEM_SOURCE) {
+            fragmentTag = PictureSelectorSystemFragment.TAG;
+            targetFragment = PictureSelectorSystemFragment.newInstance();
+        } else if (modeTypeSource == PictureConfig.MODE_TYPE_EXTERNAL_PREVIEW_SOURCE) {
+            if (PictureSelectionConfig.onInjectActivityPreviewListener != null) {
+                targetFragment = PictureSelectionConfig.onInjectActivityPreviewListener.onInjectPreviewFragment();
+            }
+            if (targetFragment != null) {
+                fragmentTag = ((PictureSelectorPreviewFragment) targetFragment).getFragmentTag();
+            } else {
+                fragmentTag = PictureSelectorPreviewFragment.TAG;
+                targetFragment = PictureSelectorPreviewFragment.newInstance();
+            }
+            int position = getIntent().getIntExtra(PictureConfig.EXTRA_PREVIEW_CURRENT_POSITION, 0);
+            ArrayList<LocalMedia> previewResult = SelectedManager.getSelectedPreviewResult();
+            ArrayList<LocalMedia> previewData = new ArrayList<>(previewResult);
+            boolean isDisplayDelete = getIntent()
+                    .getBooleanExtra(PictureConfig.EXTRA_EXTERNAL_PREVIEW_DISPLAY_DELETE, false);
+            ((PictureSelectorPreviewFragment) targetFragment).setExternalPreviewData(position, previewData.size(), previewData, isDisplayDelete);
+        } else {
+            fragmentTag = PictureOnlyCameraFragment.TAG;
+            targetFragment = PictureOnlyCameraFragment.newInstance();
+        }
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        Fragment fragment = supportFragmentManager.findFragmentByTag(fragmentTag);
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction().remove(fragment).commitAllowingStateLoss();
+        }
+        FragmentInjectManager.injectSystemRoomFragment(supportFragmentManager, fragmentTag, targetFragment);
+    }
+
+    @SuppressLint("RtlHardcoded")
+    private void setActivitySize() {
+        Window window = getWindow();
+        window.setGravity(Gravity.LEFT | Gravity.TOP);
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.x = 0;
+        params.y = 0;
+        params.height = 1;
+        params.width = 1;
+        window.setAttributes(params);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        PictureSelectionConfig config = PictureSelectionConfig.getInstance();
+        int modeTypeSource = getIntent().getIntExtra(PictureConfig.EXTRA_MODE_TYPE_SOURCE, 0);
+        if (modeTypeSource == PictureConfig.MODE_TYPE_EXTERNAL_PREVIEW_SOURCE && !config.isPreviewZoomEffect) {
+            PictureWindowAnimationStyle windowAnimationStyle = PictureSelectionConfig.selectorStyle.getWindowAnimationStyle();
+            overridePendingTransition(0, windowAnimationStyle.activityExitAnimation);
+        } else {
+            overridePendingTransition(0, R.anim.ps_anim_fade_out);
+        }
+    }
+}
